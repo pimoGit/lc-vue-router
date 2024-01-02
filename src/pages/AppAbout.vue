b179b8b9c8736a60b27104ce7daa2969d328a45a
Simone <info@simoneicardi.com>
2024-01-02 16:21:30 +0100

    creazione pages

---
+++ src/pages/AppAbout.vue
@@ -0,0 +1,13 @@
+<script>
+export default {
+    name: "AppAbout"
+}
+</script>
+
+<template>
+    <section>
+        <h2 class="mt-5 mb-3">About Us internal page</h2>
+    </section>
+</template>
+
+<style scoped></style>
---
+++ src/pages/AppHome.vue
@@ -0,0 +1,13 @@
+<script>
+export default {
+    name: "AppHome"
+}
+</script>
+
+<template>
+    <section>
+        <h2 class="mt-5 mb-3">Homepage</h2>
+    </section>
+</template>
+
+<style scoped></style>
