01b397faf31e5e4c11753137c495a776144cb15c
Simone <info@simoneicardi.com>
2024-01-02 16:45:55 +0100

    aggiunta router-links

--- src/components/HeaderComp.vue
+++ src/components/HeaderComp.vue
@@ -7,6 +7,20 @@ export default {
 
 <template>
     <h1>This is a header</h1>
+    <nav>
+        <ul>
+            <li>
+                <router-link :to="{ name: 'home' }" class="nav-link">
+                    Home
+                </router-link>
+            </li>
+            <li>
+                <router-link :to="{ name: 'about' }" class="nav-link">
+                    About Us
+                </router-link>
+            </li>
+        </ul>
+    </nav>
 </template>
 
 <style lang="scss" scoped>
