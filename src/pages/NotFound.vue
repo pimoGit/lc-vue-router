b26e3f070c062ab2d771b3888e4fa12471f530f0
Simone <info@simoneicardi.com>
2024-01-02 16:31:59 +0100

    aggiunta notfound

---
+++ src/pages/NotFound.vue
@@ -0,0 +1,13 @@
+<script>
+export default {
+    name: "NotFound"
+}
+</script>
+
+<template>
+    <section>
+        <h2 class="mt-5 mb-3">NOT FOUND 404</h2>
+    </section>
+</template>
+
+<style scoped></style>
--- src/router.js
+++ src/router.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router';
 
 import AppHome from './pages/AppHome.vue';
 import AppAbout from './pages/AppAbout.vue';
+import NotFound from './pages/NotFound.vue';
 
 const router = createRouter({
     history: createWebHistory(),
@@ -16,6 +17,12 @@ const router = createRouter({
             name: 'about',
             component: AppAbout
         },
+        {
+            path: '/:pathMatch(.*)*',
+            name: 'NotFound',
+            component: NotFound
+        },
+
     ]
 });
 
